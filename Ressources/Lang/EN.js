decbc1326283ed1cbcaa2b52503e2cbb87e1cb59
DylanPerdigao <32035460+DylanPerdigao@users.noreply.github.com>
2021-01-15 17:31:21 +0000

    init commit

---
+++ Ressources/Lang/EN.js
@@ -0,0 +1,87 @@
+function ENG(){
+// Name of the player
+QEVN = "What’s your name?"
+// Places
+HC = "Hautchâtel (Center) "
+HS ="Hautchâtel (South)"
+CS = "Way of Fir trees"
+DOM = "Home"
+LAB = "Laboratory of Hautchâtel "
+// House
+M1 = ", the director of the laboratory asked me for you to visit him<br><br> <button onclick='vrai()'> Ok </button>"
+M2 = " He’s waiting for you in his laboratory in the east of the district "
+M3= " Did you visit the laboratory’s director? <br><br> <button onclick='vrai()'>Yes</button> <button onclick='faux()'>No</button>"
+M4="Liar!"
+M5=" Why aren’t you going? Go, he’s waiting for you!"
+M6=" Is everything ok? <br><br> <button onclick='vrai()'>Yes</button> <button onclick='faux()'>No</button>"
+M7=" It's good, continue your adventure!"
+M8=" Don’t worry, you’re just going through a hard time. Everything will be alright"
+// Street
+TF1 ="Hello "
+TF2 =", excuse me but the football pitch is complete, you cannot play with us..."
+R1 =" This street is closed because we’re going to begin roadworks"
+// Random
+PF1_1 = "Recently, there was a photographer whose photo was stolen by the bank to put it on banknotes"
+PF2_1 = "Yes, it is for the new 100$ bills"
+
+PF2_2 = "Did you watch the TV news yesterday?"
+PF1_2 = "No, what happened?"
+
+PF1_3 = " There was this 9 years old kid who saved his brother from falling of his changing table"
+PF2_3 = "Fortunaly his brother was there."
+
+PF2_4 = "Next year we shall have new bank notes"
+PF1_4 = "Fortunately, the actual ones are really getting old"
+
+PF1_5 = "I’m satisfied that they thought of adding seatbelts to tractors"
+PF2_5 = "I don’t see the utility, they don’t drive very fast anyways"
+
+PF2_6 = "The electricity bill increased this month"
+PF1_6 = "Mine too"
+
+PF1_7 = "Tomorrow I’m going for a walk in the forest"
+PF2_7 = "Me too, I can come with you if you want!"
+
+PF1_8 = "I’ve read the newspaper, it’s hopeless..."
+PF2_8 = "I completely agree"
+
+PF1_9 = "The children who are on the soccer field must love playing soccer, they’ve been playing for 2 hours already"
+PF2_9 = "By speaking about soccer, do you know when will the next match of Champions League occur?"
+
+PF1_10 = "It’s impressive seeing the evolution of the technology"
+PF2_10 = "Yes, it’s going very fast"
+
+// Random 2
+PF3_1 = "It is very relaxant at my old age to walk in this district of Hautchâtel";
+PF3_2 = "Your creature is injured! I can heal him..."
+PF4_1 = "You’d better visit the director of the laboratory before going any farther"
+PF4_2 = "The weather is nice today"
+// Director’s Laboratory
+DL1=", here you are! Nice to meet you! <br><br> <button onclick='vrai()'>Me too!</button>"
+DL2="I’m working on a rather crazy project! And I need your help! <br><br> <button onclick='vrai()'> Ok, what can I do to help you? </button>"
+DL3="I’m going to confide you a genetically modified creature and I want you to go to the South of the district to fight other creatures of the same type! They’re hiding in tall grass! <br><br> <button onclick='vrai()'> Why do I need to fight them? </button>"
+DL4="Your question is very relevant, I’ve inserted into it a sophisticated chip in its brain, this way I can make my analyses in real time which will help me advance in my researches! <br><br> <button onclick='vrai()'> Ok </button>"
+DL5="Have you understood or should I repeat? <br><br> <button onclick='vrai()'> Yes, I’ve understood </button> <button onclick='faux()'> No, can you repeat? </button>"
+DL6="Are you ready to help me for this task? <br><br> <button onclick='vrai()'> Yes!</button><button onclick='faux()'> No ... </button>"
+DL7="Nice! Here is the creature! <br><br> <button onclick='vrai()'>ACCEPT</button>"
+DL8="Come see me when you change your mind "
+DL9="Take care of it!"
+// Assisting Laboratory
+AL1 = "These machines need too much maintenance, they keep breaking down!"
+// Other
+DUNAVC = "Give a name to your creature"
+play = "Play"
+command = "Commands"
+language = "Languages"
+VAP = "You lost!"
+VAG = "You won!"
+NIV="Level "
+XP ="<br>Exp("
+LIFE =")<br>Life: "
+SVC = "Heal your creature by pressing SHIFT!"
+SLT = "Hello "
+JVQ = "<br> I see that "
+EXCLAM = "!"
+NOTBAD= " has quite progressed! He’s at level "
+PROGRES = " can still progress! He’s still at level "
+}
